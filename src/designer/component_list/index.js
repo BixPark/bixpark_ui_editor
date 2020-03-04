c92b46aa97c08c84ecbd5da5f529f53193db118f
Dewmal <dewmalnilanka@gmail.com>
2020-03-04 20:07:30 +0530

    Add 3 components

--- src/designer/component_list/index.js
+++ src/designer/component_list/index.js
@@ -1,7 +1,75 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import './index.css'
 import {HeroComponentDesign1} from "../components/hero/design_1";
 import ReactDOM from "react-dom";
+
+const EditorModalComponent = ({modalRef, Component, toggleModal, data, setData}) => {
+
+    useEffect(() => {
+        const overlay = document.querySelector('.modal-overlay');
+        overlay.addEventListener('click', toggleModal);
+
+        let closemodal = document.querySelectorAll('.modal-close');
+        for (let i = 0; i < closemodal.length; i++) {
+            closemodal[i].addEventListener('click', toggleModal);
+        }
+
+        document.onkeydown = function (evt) {
+            evt = evt || window.event;
+            let isEscape = false;
+            if ("key" in evt) {
+                isEscape = (evt.key === "Escape" || evt.key === "Esc")
+            } else {
+                isEscape = (evt.keyCode === 27)
+            }
+            if (isEscape && document.body.classList.contains('modal-active')) {
+                toggleModal()
+            }
+        };
+
+    });
+
+
+    return (
+        <div
+            ref={modalRef}
+            className="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+            <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
+
+            <div
+                className="modal-container bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+
+                <div
+                    className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                    <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18"
+                         height="18" viewBox="0 0 18 18">
+                        <path
+                            d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                    </svg>
+                    <span className="text-sm">(Esc)</span>
+                </div>
+
+                <div className="modal-content py-4 text-left px-6">
+                    <div className="flex justify-between items-center pb-3">
+                        <p className="text-2xl font-bold">Simple Modal!</p>
+                        <div className="modal-close cursor-pointer z-50">
+                            <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
+                                 height="18" viewBox="0 0 18 18">
+                                <path
+                                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                        </div>
+                    </div>
+
+                    <Component status={"editor"} data={data} setData={setData}/>
+
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
 
 const ComponentSelectWrapper = ({id, Component}) => {
     return (
@@ -18,9 +86,24 @@ const ComponentSelectWrapper = ({id, Component}) => {
 };
 
 const ComponentPreviewWrapper = ({id, Component}) => {
+    const modalRef = useRef();
+    const [data, setData] = useState({});
+
+    const toggleModal = () => {
+        const body = document.querySelector('body');
+        const modal = modalRef.current;
+        modal.classList.toggle('opacity-0');
+        modal.classList.toggle('pointer-events-none');
+        body.classList.toggle('modal-active');
+    };
+
+
     return (
         <div className="max-w-sm overflow-hidden shadow-lg" id={id}>
-            <Component status={"build"}/>
+            <button onClick={toggleModal}>Edit</button>
+            <Component status={"build"} data={data} setData={setData}/>
+            <EditorModalComponent modalRef={modalRef} data={data} setData={setData} toggleModal={toggleModal}
+                                  Component={Component}/>
         </div>
     )
 
@@ -37,12 +120,14 @@ export const ComponentListView = ({drake, refId}) => {
     useEffect(() => {
         if (drake) {
             drake.on("drop", function (el, target, source, sibling) {
-                const componentId = el.id;
-                const Component = getComponent(el.id);
-                let newNode = document.createElement("div");
-                // let component = React.createElement(HeroComponentDesign1, {status: "build"});
-                ReactDOM.hydrate(<ComponentPreviewWrapper Component={Component} id={componentId}/>, newNode);
-                el.parentNode.replaceChild(newNode, el);
+                if (el && el.parentNode) {
+                    const componentId = el.id;
+                    const Component = getComponent(el.id);
+                    let newNode = document.createElement("div");
+                    // let component = React.createElement(HeroComponentDesign1, {status: "build"});
+                    ReactDOM.hydrate(<ComponentPreviewWrapper Component={Component} id={componentId}/>, newNode);
+                    el.parentNode.replaceChild(newNode, el);
+                }
             });
         }
         setComponentList({
--- src/designer/components/hero/design_1/index.js
+++ src/designer/components/hero/design_1/index.js
@@ -1,37 +1,144 @@
-import React from 'react';
+import React, {useEffect, useState,useReducer} from 'react';
 import './index.css'
+import {ErrorMessage, Field, Form, Formik} from "formik";
+
+class HeroData {
+    name = "The Coldest Sunset";
+    description = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et\n" +
+        "                    perferendis eaque, exercitationem praesentium nihil.";
+    image = "https://tailwindcss.com/img/card-top.jpg"
+}
+
+const ComponentView = ({data}) => {
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg w-full">
+            <img className="w-full" src={data.image} alt="Sunset in the mountains"/>
+            <div className="px-6 py-4">
+                <div className="font-bold text-xl mb-2">{data.name}</div>
+                <p className="text-gray-700 text-base">
+                    {data.description}
+                </p>
+            </div>
+        </div>
+    );
+};
+const ComponentDataEditor = (data, setData) => {
+    return (
+        <>
+            <Formik
+                initialValues={{name: "Name"}}
+                onSubmit={(values, {setSubmitting}) => {
+                    setData(
+                        values
+                    )
+                }}
+            >
+                {({isSubmitting}) => (
+                    <div className={"w-full max-w-lg"}>
+                        <Form>
+                            <div className="flex flex-wrap -mx-3 mb-6">
+                                <Field name="name">
+
+                                    {({
+                                          field, // { name, value, onChange, onBlur }
+                                          form: {touched, errors}, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+                                          meta,
+                                      }) => (
+                                        <>
+                                            <label
+                                                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2"
+                                                htmlFor="grid-first-name">
+                                                First Name
+                                            </label>
+                                            <input
+                                                className="appearance-none block
+                                             w-full bg-gray-200 text-gray-700 border
+                                             rounded py-3 px-4
+                                              mb-3 leading-tight focus:outline-none focus:bg-white"
+                                                placeholder="Jane"  {...field}/>
+                                            {meta.touched && meta.error && (
+                                                <p className="text-red-500 text-xs italic">{meta.error}</p>
+                                            )}
+
+                                        </>
+                                    )}
 
 
-export const HeroComponentDesign1 = ({key, status}) => {
-    console.log(status);
-    if (status === "preview") {
-        return (
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"/>
-            </div>
-        );
-    } else {
-        return (
-            <div className="max-w-sm rounded overflow-hidden shadow-lg">
-                <img className="w-full" src="https://tailwindcss.com/img/card-top.jpg" alt="Sunset in the mountains"/>
-                <div className="px-6 py-4">
-                    <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-                    <p className="text-gray-700 text-base">
-                        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et
-                        perferendis eaque, exercitationem praesentium nihil.
-                    </p>
-                </div>
-                <div className="px-6 py-4">
-                    <span
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#photography</span>
-                    <span
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">#travel</span>
-                    <span
-                        className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700">#winter</span>
-                </div>
-            </div>
-        );
+                                </Field>
+                            </div>
+                            <div className="flex flex-wrap -mx-3 mb-6">
+                                <Field name="description">
+
+                                    {({
+                                          field, // { name, value, onChange, onBlur }
+                                          form: {touched, errors}, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+                                          meta,
+                                      }) => (
+                                        <>
+                                            <label
+                                                className="block uppercase tracking-wide text-gray-700 text-xs font-bold mb-2">
+                                                Description
+                                            </label>
+                                            <input
+                                                className="appearance-none block
+                                             w-full bg-gray-200 text-gray-700 border
+                                             rounded py-3 px-4
+                                              mb-3 leading-tight focus:outline-none focus:bg-white"
+                                                placeholder="Description"  {...field}/>
+                                            {meta.touched && meta.error && (
+                                                <p className="text-red-500 text-xs italic">{meta.error}</p>
+                                            )}
+
+                                        </>
+                                    )}
+
+
+                                </Field>
+                            </div>
+
+                            <div className="flex justify-end pt-2">
+                                <button
+                                    disabled={isSubmitting}
+                                    type={"submit"}
+                                    className="px-4 bg-transparent p-3 rounded-lg text-indigo-500 hover:bg-gray-100 hover:text-indigo-400 mr-2">Action
+                                </button>
+                                <button
+                                    className="modal-close px-4 bg-indigo-500 p-3 rounded-lg text-white hover:bg-indigo-400">Close
+                                </button>
+                            </div>
+                        </Form>
+                    </div>
+                )}
+            </Formik>
+
+
+        </>
+    );
+};
+
+const ComponentPreview = () => {
+    return (
+        <div className="max-w-sm rounded overflow-hidden shadow-lg">
+            <h1>Header Component</h1>
+        </div>
+    );
+};
+
+
+export const HeroComponentDesign1 = ({key, status, data, setData}) => {
+    const hero = useState(new HeroData());
+    useEffect(() => {
+        if (!data && setData) {
+            setData(hero);
+        }
+    }, [data]);
+
+    switch (status) {
+        case "preview":
+            return <ComponentPreview/>;
+        case "editor":
+            return <ComponentDataEditor data={hero} setData={setData}/>;
+        default:
+            return <ComponentView data={hero}/>
     }
-
-
 };
