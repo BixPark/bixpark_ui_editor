5a1cf558c8ef2bd91dfbc0fd3bcf75c05f793e69
Dewmal <dewmalnilanka@gmail.com>
2020-03-05 09:09:04 +0530

    Update editor view

--- src/designer/components/editor_base/EditorForm.js
+++ src/designer/components/editor_base/EditorForm.js
@@ -146,7 +146,7 @@ const EditorForm = ({formData, data, updateData}) => {
             }}
         >
             {({isSubmitting, setFieldValue, values}) => (
-                <div className={"w-full max-w-lg"}>
+                <div className="w-full max-w-lg h-screen max-h-screen">
                     <Form>
                         {Object.entries(formData).map(([key, value]) => {
                             switch (value.type) {
@@ -221,33 +221,39 @@ export const EditorModalComponent = ({modalRef, Component, toggleModal, data, se
 
     return (
         <div ref={modalRef}
-             className="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
-            <div className="modal-overlay absolute w-full h-full bg-white opacity-95"></div>
+            className="modal opacity-0 pointer-events-none fixed w-full h-full top-0 left-0 flex items-center justify-center">
+            <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>
 
-            <div className="modal-container fixed w-full h-full z-50 overflow-y-auto ">
+            <div
+                className="modal-container bg-white w-3/4 mx-auto rounded shadow-lg z-50 overflow-y-auto">
 
                 <div
-                    className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-black text-sm z-50">
-                    <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
+                    className="modal-close absolute top-0 right-0 cursor-pointer flex flex-col items-center mt-4 mr-4 text-white text-sm z-50">
+                    <svg className="fill-current text-white" xmlns="http://www.w3.org/2000/svg" width="18" height="18"
                          viewBox="0 0 18 18">
                         <path
                             d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
                     </svg>
-                    (Esc)
+                    <span className="text-sm">(Esc)</span>
                 </div>
 
-
-                <div className="modal-content container mx-auto h-auto text-left p-4">
-
-                    <div className="flex justify-between items-center pb-2">
-                        <p className="text-2xl font-bold">Full Screen Modal!</p>
+                <div className="modal-content py-4  text-left px-6">
+                    <div className="flex justify-between items-center pb-3">
+                        <p className="text-2xl font-bold">Simple Modal!</p>
+                        <div className="modal-close cursor-pointer z-50">
+                            <svg className="fill-current text-black" xmlns="http://www.w3.org/2000/svg" width="18"
+                                 height="18" viewBox="0 0 18 18">
+                                <path
+                                    d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"></path>
+                            </svg>
+                        </div>
                     </div>
 
                     <EditorForm data={data} formData={formData} updateData={setData}/>
 
-
                 </div>
             </div>
         </div>
+
     )
 };
