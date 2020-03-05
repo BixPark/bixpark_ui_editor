250c4816d659bee7964d5722020bde6c7f672295
Dewmal <dewmalnilanka@gmail.com>
2020-03-05 11:52:38 +0530

    Optimize designer view

--- src/designer/designer_view/index.js
+++ src/designer/designer_view/index.js
@@ -1,25 +1,48 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './index.css'
 
 
 export const DesignView = ({refId}) => {
 
+    const [layoutSize, setLayoutSize] = useState("browser--desktop");
+
+    const changeLayoutSize = (size) => {
+        setLayoutSize(size);
+    };
+
     const exportToHtml = () => {
         const content = refId.current;
         const htmlContent = content.innerHTML;
     };
-    return (<div className="p-4 h-screen max-h-screen">
-            <div className="mx-8 mb-2 border-solid border-grey-light rounded border shadow-sm">
-                <div className="bg-gray-200 px-2 py-3 border-solid border-grey-light border-b">
-                    <button
-                        onClick={exportToHtml}> Export
-                    </button>
-                </div>
-                <div ref={refId} className="ui-designer h-full h-16 container overflow-scroll">
+    return (<div className="flex flex-wrap py-2">
+            <div className={`ml-auto  mr-auto ${layoutSize}`}>
+                <div className="border-solid border-grey-light rounded border shadow">
+                    <div className="bg-gray-200 px-2 py-3 border-solid border-grey-light border-b">
+                        <div className="flex flex-wrap">
+                            <div class="flex-auto items-end">
+                                <button className="py-2 px-4 text-gray-600 text-sm focus:outline-none"
+                                        onClick={() => {
+                                            changeLayoutSize("browser--mobile")
+                                        }}>
+                                    <i className="fa fa-mobile-alt"></i>
+                                </button>
+                                <button className="py-2 px-4 text-gray-600 text-sm focus:outline-none" onClick={() => {
+                                    changeLayoutSize("browser--tablet")
+                                }}>
+                                    <i className="fa fa-tablet-alt"></i>
+                                </button>
+                                <button className="py-2 px-4 text-gray-600 text-sm focus:outline-none" onClick={() => {
+                                    changeLayoutSize("browser--desktop")
+                                }}>
+                                    <i className="fa fa-desktop"></i>
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                    <div ref={refId} className="ui-designer h-full h-16 container overflow-y-auto py-2 px-3">
+                    </div>
                 </div>
             </div>
-
-
         </div>
     );
 };
